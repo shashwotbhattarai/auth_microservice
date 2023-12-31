56d72562bcbed62cb1a2a342838bf61b2cdd582d
shashwot-vg <shashwot@virtual-gravity.com>
2023-12-31 18:00:12 +0545

    refactored sqs.service.test.ts and added sonar

--- src/__tests__/sqs.service.test.ts
+++ src/__tests__/sqs.service.test.ts
@@ -38,31 +38,18 @@ describe("Sqs service", () => {
 		expect(result.status).toBe(200);
 	});
 
-	// test("sqs error occures", async () => {
-	// 	//mock all dependencies
-	// 	const generateUniqueId = jest.fn().mockReturnValue("mocked-unique-id");
+	test("sqs error occures", async () => {
+		//mock all dependencies
+		const generateUniqueId = jest.fn().mockReturnValue("mocked-unique-id");
 
-	// 	sqsClientMock.on(SendMessageCommand).resolves({
-	// 		$metadata: {
-	// 			httpStatusCode: 500,
-	// 			requestId: "6b3e8e90-5cc2-5ab0-8271-d36853268a0e",
-	// 			extendedRequestId: undefined,
-	// 			cfId: undefined,
-	// 			attempts: 1,
-	// 			totalRetryDelay: 0,
-	// 		},
-	// 		MD5OfMessageAttributes: "8364c9f86971581747998a1be1bc61a7",
-	// 		MD5OfMessageBody: "3f4c3c36d1208f4bd292aabf8b295141",
-	// 		MessageId: "55029f88-98f9-41ef-88b0-e777c7da7629",
-	// 		SequenceNumber: "74222882815101630464",
-	// 	});
-	// 	const emailPayload = {
-	// 		to: "babudallay@gmail.com",
-	// 		subject: "new user created",
-	// 		text: "your user has been created",
-	// 	};
-	// 	const result = await new SQS_Service().sendMessageToQueue(emailPayload, sqsClientMock);
+		sqsClientMock.on(SendMessageCommand).rejects(new Error("SQS Error"));
+		const emailPayload = {
+			to: "babudallay@gmail.com",
+			subject: "new user created",
+			text: "your user has been created",
+		};
+		const result = await new SQS_Service().sendMessageToQueue(emailPayload, sqsClientMock);
 
-	// 	expect(result.status).toBe(500);
-	// });
+		expect(result.status).toBe(500);
+	});
 });
