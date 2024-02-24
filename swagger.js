a3d6a5665ba03a2d98157ed894a93d50295674b1
shashwotbhattarai <shashwot.tech@gmail.com>
2024-02-25 01:32:35 +0545

    made port allcoation dynamic

--- swagger.js
+++ swagger.js
@@ -1,12 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const swaggerAutogen = require("swagger-autogen")();
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const dotenv = require("dotenv");
+dotenv.config();
 
 const doc = {
   info: {
     title: "My API",
     description: "Description",
   },
-  host: "localhost:3001",
+  host: `localhost:${process.env.PORT}`,
 };
 
 const outputFile = "./swagger-output.json";
