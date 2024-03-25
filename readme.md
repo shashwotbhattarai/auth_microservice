d344c3bddd6f51bd575b7282551a951eb2cd2a4d
shashwotbhattarai <shashwot.tech@gmail.com>
2024-03-25 11:05:24 +0545

    updated readme and test case

--- readme.md
+++ readme.md
@@ -29,21 +29,60 @@ This microservice is designed to handle all authentication-related operations wi
 
    ```bash
    git clone https://github.com/ShashwotBhattarai/auth_microservice.git
-
    ```
 
 2. Install NPM packages:
 
    ```bash
    npm install
-
    ```
 
 3. Run the application:
 
    ```bash
    npm run start
-
    ```
 
-4. Application will start on port specified in env variable.
+4. Application will start on port specified in env variable:
+
+   ```bash
+   DATABASEURI=
+   JWTSECRET=
+   AWS_ACCESS_KEY_ID=
+   AWS_SECRET_ACCESS_KEY=
+   AWS_REGION=
+   SQS_QUEUE_URL=
+   PATH=
+   Access_Control_Allow_Origin=
+   ENV=
+   PORT=
+   ```
+
+5. To test apis:
+
+   ```bash
+   Health check API:
+
+   curl --location 'http://localhost:3001/auth/health' \
+      --data ''
+
+
+   Signup API:
+
+   curl --location 'http://localhost:3001/auth/signup' \
+   --header 'username: hellowww' \
+   --header 'password: Password@1234' \
+   --header 'Content-Type: application/json' \
+   --data-raw '{
+      "email": "babudallay@gmail.com",
+      "role": "candidate"}'
+
+
+   Login API:
+
+   curl --location --request POST 'http://localhost:3001/auth/login' \
+   --header 'username: hello' \
+   --header 'password: Password@1234' \
+   --data ''
+
+   ```
--- src/middlewares/signupInputValidation.middleware.spec.ts
+++ src/middlewares/signupInputValidation.middleware.spec.ts
@@ -5,15 +5,8 @@ import supertest from "supertest";
 const validateSignupMiddleware = new ValidateSignupMiddleware()
   .validateSignupMiddleware;
 
-// Mocking the dependencies
 jest.mock("../configs/logger.config", () => ({
   error: jest.fn(),
-}));
-
-jest.mock("../constants/validateSignUpInput.schema", () => ({
-  validateSignupInput: {
-    validate: jest.fn(),
-  },
 }));
 
 const app = express();
@@ -24,45 +17,23 @@ app.post("/signup", validateSignupMiddleware, (req, res) => {
 
 describe("validateSignupMiddleware", () => {
   it("passes validation for correct signup data", async () => {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    require("../constants/validateSignUpInput.schema").validateSignupInput.validate.mockReturnValueOnce(
-      {},
-    );
-
     const response = await supertest(app)
       .post("/signup")
       .set("username", "testuser")
       .set("password", "password123")
-      .send({ email: "test@example.com", role: "user" });
+      .send({ email: "test@example.com", role: "candidate" });
 
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe("Passed validation");
   });
 
-  it("returns error for invalid signup data", async () => {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    require("../constants/validateSignUpInput.schema").validateSignupInput.validate.mockReturnValueOnce(
-      {
-        error: {
-          details: [
-            {
-              message: "Invalid input data",
-            },
-          ],
-        },
-      },
-    );
-
+  it("fails validation for incorrect signup data", async () => {
     const response = await supertest(app)
       .post("/signup")
       .set("username", "testuser")
       .set("password", "")
-      .send({ email: "test@example", role: "user" });
+      .send({ email: "test@example", role: "recruiter" });
 
     expect(response.statusCode).toBe(400);
-    expect(response.body).toEqual({
-      status: "error",
-      message: "Invalid input data",
-    });
   });
 });
