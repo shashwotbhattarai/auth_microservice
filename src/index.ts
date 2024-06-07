b4e3b633854117a1a5c2432cebf0bcfb73e27a5f
shashwotbhattarai <shashwot.tech@gmail.com>
2024-06-08 01:12:40 +0545

    lint fuckup

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import app from "./configs/express.config";
 
 const port = envVars.PORT;
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-console.log("sfsfsdfsfsfasfasdf")
+console.log("lint fuck up");
 
 connectToDatabase();
 app.use("/auth", authRoute);
