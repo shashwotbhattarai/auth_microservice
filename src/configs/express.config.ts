a94757e23f55cfe0f257b5f27e14481a7e01a5a8
shashwotbhattarai <shashwot.tech@gmail.com>
2024-06-08 14:27:26 +0545

    intriduced an sonar issue for testing sonar job

--- src/configs/express.config.ts
+++ src/configs/express.config.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import { envVars } from "./envVars.config";
 
 const app: Express = express();
-app.disable("x-powered-by");
+// app.disable("x-powered-by");
 const corsOptions = {
   origin: [envVars.Access_Control_Allow_Origin_URL as string],
 };
