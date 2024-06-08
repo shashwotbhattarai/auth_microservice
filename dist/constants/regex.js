98e5b401205ea11a40894e2a8589490172933aa2
shashwotbhattarai <shashwot.tech@gmail.com>
2024-06-09 01:57:09 +0545

    removed dist

--- dist/constants/regex.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pwRegex = exports.usernameRegex = void 0;
-exports.usernameRegex = "^[A-Za-z0-9!@#$%^&*()_+=-]{3,30}$";
-exports.pwRegex = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}";
-//# sourceMappingURL=regex.js.map
