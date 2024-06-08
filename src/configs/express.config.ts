e1721625faee1a5330c81d464f38d56a2413dcfe
shashwotbhattarai <shashwot.tech@gmail.com>
2024-06-08 16:27:28 +0545

    debug4

--- src/configs/express.config.ts
+++ src/configs/express.config.ts
@@ -6,10 +6,10 @@ import { envVars } from "./envVars.config";
 const app: Express = express();
 // app.disable("x-powered-by");
 
-const corsOptions = {
-  origin: [envVars.Access_Control_Allow_Origin_URL as string],
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: [envVars.Access_Control_Allow_Origin_URL as string],
+// };
+app.use(cors());
 
 app.use(bodyParser.json());
 
