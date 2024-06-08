a0e6769e45cadf9ff548c897117429434299eda5
shashwotbhattarai <shashwot.tech@gmail.com>
2024-06-08 16:29:37 +0545

    debug5

--- src/configs/express.config.ts
+++ src/configs/express.config.ts
@@ -1,7 +1,7 @@
 import express, { Express } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import { envVars } from "./envVars.config";
+// import { envVars } from "./envVars.config";
 
 const app: Express = express();
 // app.disable("x-powered-by");
