ab0f223d313e2ab642b8b1cf64f0fe30aa6849da
shashwotbhattarai <shashwot.tech@gmail.com>
2024-06-08 17:04:42 +0545

    debuged

--- src/configs/express.config.ts
+++ src/configs/express.config.ts
@@ -1,16 +1,15 @@
 import express, { Express } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-// import { envVars } from "./envVars.config";
-//wablabadubdub
+import { envVars } from "./envVars.config";
 
 const app: Express = express();
-// app.disable("x-powered-by");
+app.disable("x-powered-by");
 
-// const corsOptions = {
-//   origin: [envVars.Access_Control_Allow_Origin_URL as string],
-// };
-app.use(cors());
+const corsOptions = {
+  origin: [envVars.Access_Control_Allow_Origin_URL as string],
+};
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 
