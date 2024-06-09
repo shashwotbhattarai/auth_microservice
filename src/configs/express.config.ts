c64732bbb0b130c36179b59af5f1855707313402
shashwotbhattarai <shashwot.tech@gmail.com>
2024-06-09 19:17:05 +0545

    introduced sonar issue

--- src/configs/express.config.ts
+++ src/configs/express.config.ts
@@ -1,15 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import express, { Express } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { envVars } from "./envVars.config";
 
 const app: Express = express();
-app.disable("x-powered-by");
+// app.disable("x-powered-by");
 
-const corsOptions = {
-  origin: [envVars.Access_Control_Allow_Origin_URL as string],
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: [envVars.Access_Control_Allow_Origin_URL as string],
+// };
+app.use(cors());
 
 app.use(bodyParser.json());
 
