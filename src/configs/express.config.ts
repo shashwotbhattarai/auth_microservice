bcd9b2172e94632a0ed137adb42edbe95abb10cd
shashwotbhattarai <shashwot.tech@gmail.com>
2024-06-10 13:21:37 +0545

    intoduced sonar issue

--- src/configs/express.config.ts
+++ src/configs/express.config.ts
@@ -1,15 +1,15 @@
 import express, { Express } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import { envVars } from "./envVars.config";
+// import { envVars } from "./envVars.config";
 
 const app: Express = express();
 app.disable("x-powered-by");
 
-const corsOptions = {
-  origin: [envVars.Access_Control_Allow_Origin_URL as string],
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: [envVars.Access_Control_Allow_Origin_URL as string],
+// };
+app.use(cors());
 
 app.use(bodyParser.json());
 
