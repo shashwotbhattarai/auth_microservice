3c369283f5f1f064bce16b11829f905ce8c77ad0
shashwotbhattarai <shashwot.tech@gmail.com>
2024-06-10 22:13:12 +0545

    jenkins pipeline with github webhook working

--- src/configs/express.config.ts
+++ src/configs/express.config.ts
@@ -4,17 +4,14 @@ import cors from "cors";
 import { envVars } from "./envVars.config";
 
 const app: Express = express();
+
 app.disable("x-powered-by");
-
-
-
-
 
 const corsOptions = {
   origin: [envVars.Access_Control_Allow_Origin_URL as string],
 };
-app.use(cors(corsOptions));
 
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 export default app;
