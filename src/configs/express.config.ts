c2e685e25eca7ce6fe6d25329569355248d229b1
shashwotbhattarai <shashwot.tech@gmail.com>
2024-06-11 19:04:24 +0545

    test: testing sonar issues

--- src/configs/express.config.ts
+++ src/configs/express.config.ts
@@ -1,17 +1,17 @@
 import express, { Express } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import { envVars } from "./envVars.config";
+// import { envVars } from "./envVars.config";
 
 const app: Express = express();
 
 app.disable("x-powered-by");
 
-const corsOptions = {
-  origin: [envVars.Access_Control_Allow_Origin_URL as string],
-};
+// const corsOptions = {
+//   origin: [envVars.Access_Control_Allow_Origin_URL as string],
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 
 export default app;
